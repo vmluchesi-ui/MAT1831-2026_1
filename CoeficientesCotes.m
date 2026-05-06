252ba19a9e992912b5dd367cd3213ef4d6543852
vmluchesi-ui <vmluchesi@gmail.com>
2026-05-06 12:56:41 -0300

    Add files via upload

--- CoeficientesCotes.m
+++ CoeficientesCotes.m
@@ -10,10 +10,11 @@ function [Den, VetC, Info] = CoeficientesCotes(n)
     end
 
     d = [2 6 8 90 288 840 17280 28350 89600 598752];
-    w=calcularPesosNewtonCotes(n);
+%c=calcularPesosNewtonCotes(n);
+    c = [ 1 1 4 1 3 7 32 12 19 75 50 41 216 27 272 751 3577 1323 2989 989 5888 -928 10496 -4540 2857 15741 1080 19344  5778 16067 106300 -48525 272400 -260550 427368 ];
 
     Den = d(n);
-    c(n)=w/d(n);
+
     p = floor(n*(n+2)/4) + mod(n,2);
     n12 = floor((n+1)/2) - 1;
 
